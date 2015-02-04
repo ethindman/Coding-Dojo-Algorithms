2de581423c172e13b9203d74f86cfb55373cca02
Elliott Hindman <portokaliajapan@gmail.com>
2015-02-04 12:21:24 -0800

    Refactors logic for testing user input for common mistakes outside forLoop(), Adds title to page and second and third algorithms

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Coding-Dojo-Algorithms
-Taking the introductory algorithms from Coding Dojo's pre-bootcamp courswork and turning them into functional examples that users can play with and learn from
+Taking the introductory algorithms from Coding Dojo's pre-bootcamp courswork and turning them into functional examples that users can play with and learn from.
+
+
--- js/main.js
+++ js/main.js
@@ -1,42 +1,58 @@
-function print255(outputId){
-	var quantity = $('input').val();
+//jQuery functions wll toggle notes section and clear button for each lesson-node
+$(document).ready(function() {
+	$('.show-hide').click(function() {
+    	$(this).closest('.lesson-node').find('.lesson-notes').slideToggle();
+    });
+    $('.clear').click(function() {
+        var $this = $(this).closest('.lesson-node');
+        $this.find('.output').empty();
+        $this.find('.lesson-notes').hide();
+       	$this.find('input').val("");
+    });
+});
+
+//Runs a for loop with pre-specified initial and increment values
+function forLoop(init, increment, inputId, outputId){
+	var quantity = $(inputId).val();
 	var formatText = removePunc(quantity);
 	$(outputId).empty();
-	
-	if (formatText == "") {
-		errorMessage(outputId, "Oops! You forgot to type a number!")
-	}
-	else if (formatText <= 0) {
-		errorMessage(outputId, "Oh! Did I mention zeros and negative numbers don't work here?");
-	}
-	else if (formatText > 5000 ){
-		errorMessage(outputId, "Whoa! That's a huge number to print!! Why not choose a number less than 5,000?");
-	}
-	else if (isNaN(formatText)) {
-		errorMessage(outputId, "Wait a second... That's not a number...");
-	}
-	else {
-		for(i=1; i<=formatText; i++) {
+	 if(!testInput(formatText, outputId))
+        {
+          return;
+        }
+		for(i=init; i<=formatText; i=i+increment) {
 			$(outputId).append("  "+i);
 		}
+		$(outputId).append("<p class='success-msg'>Algorithm printed succesfully!<p>");
+}
+
+//Tests input for common input errors
+function testInput(input, outputId) {
+  if (input !== "" && input < 5000 && input > 0) {
+      return true;
+  	} 
+  	else if (input == "") {
+		errorMessage(outputId, "Oops! You forgot to type a number!")
 	}
+	else if (input <= 0) {
+		errorMessage(outputId, "Oh! Did I mention zeros and negative numbers don't work here?");
+	}
+	else if (input > 5000 ){
+		errorMessage(outputId, "Whoa! That number is too huge!! Why not choose a number less than 5,000?");
+	}
+	else if (isNaN(input)) {
+		errorMessage(outputId, "Wait a second... Is that even a number?");
+	}
+  	return false;
 }
 
-function showWork(algorithmId){
-	$(algorithmId).slideToggle();
-}
-
+//Prints error message to nearest output area
 function errorMessage(outputId, message) {
 	$(outputId).html("<p class='error-msg'>"+message+"</p>");
 	$('input').val("");
 }
 
-function clearOutput(outputId, algorithmId) {
-	$(outputId).empty();
-	$('input').val("");
-	$(algorithmId).hide();
-}
-
+//Removes commas from user input
 function removePunc(userInput) {
 	userInput = userInput.replace(/,/g,"");
 	return userInput;
